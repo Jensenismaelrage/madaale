924e2630d053f764c9d143b5f35932e9411f5ef1
Jensenismaelrage <90767793+Jensenismaelrage@users.noreply.github.com>
2021-11-23 12:58:35 +0100

    second apploud
    keep applauding

---
+++ main.js
@@ -0,0 +1,8 @@
+function todolist(){
+    var item = document.getElementById("todoinput").value;
+    var text = document.createTextNode(item);
+    var newItem = document.createElement("li");
+    newItem.appendChild(text);
+    document.getElementById("todolist").appendChild(newItem);
+
+}
